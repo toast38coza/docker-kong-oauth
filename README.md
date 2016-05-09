696e8dea2dccf4484260c44dee24ca2a7d558450
toast38coza <info@38.co.za>
2016-05-09 21:48:12 +0200

    update the readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # docker-kong-oauth
 An example of implementing Kong's oauth plugin with docker
+
+**Warning:** Work in Progress
+
+**Progress**
+
+* [x] MicroService's with docker-compose
+* [x] Manually register all services + verify how OAuth plugin functions
+* [.] Create actual authentication backend which interacts with Kong OAuth using Django's user management system
+* [.] Automate initial API registration etc via shell script / python script / go script. 
+* [.] [Profit](http://www.lstreetc.com/wp-content/uploads/2014/04/Underpants-Gnomes.png)
+* [.] Write linked blog post
+
+
+
+**Questions**
+
+1. **Q:** If I get an access token for a downstream API, is it useable on other downstream APIs? <br/>
+**A:** Yes
+1. ...
---
+++ register.sh
@@ -0,0 +1,42 @@
+curl -i -X POST \
+  --url http://docker.local:8001/apis/ \
+  --data 'name=service1' \
+  --data 'upstream_url=http://docker.local:8003/' \
+  --data 'request_host=service1.com' \
+  --data 'request_path=/service1' \
+  --data 'strip_request_path=true' 
+
+curl -i -X POST \
+  --url http://docker.local:8001/apis/ \
+  --data 'name=service2' \
+  --data 'upstream_url=http://docker.local:8004' \
+  --data 'request_host=service2.com' \
+  --data 'request_path=/service2' \
+  --data 'strip_request_path=true'   
+
+curl -X POST http://docker.local:8001/apis/baa777ec-ae40-46f3-98eb-e4ee00474743/plugins \
+    --data "name=oauth2"  
+
+{"api_id":"baa777ec-ae40-46f3-98eb-e4ee00474743","id":"e80d4743-26b5-4846-8dce-fbb0e393766c","created_at":1462821458000,"enabled":true,"name":"oauth2","config":{"mandatory_scope":false,"token_expiration":7200,"enable_implicit_grant":false,"hide_credentials":false,"provision_key":"c776b1cdd4974e85a7fc8e1390ce7769","accept_http_if_already_terminated":false,"enable_authorization_code":true,"enable_client_credentials":false,"enable_password_grant":false}}
+
+curl -X POST http://docker.local:8001/apis/8832a1f2-a427-4ba8-bee8-e34fa894d682/plugins \
+    --data "name=oauth2"  
+{"api_id":"8832a1f2-a427-4ba8-bee8-e34fa894d682","id":"3e039cc5-fe59-49c6-a804-f69631b5fc96","created_at":1462821497000,"enabled":true,"name":"oauth2","config":{"mandatory_scope":false,"token_expiration":7200,"enable_implicit_grant":false,"hide_credentials":false,"provision_key":"7656a7f4dee345a6a1270a273c099480","accept_http_if_already_terminated":false,"enable_authorization_code":true,"enable_client_credentials":false,"enable_password_grant":false}}    
+
+curl -X POST http://docker.local:8001/consumers/ \
+    --data "username=user123" \
+    --data "custom_id=1"
+
+{"custom_id":"1","username":"user123","created_at":1462821574000,"id":"26550d58-68f0-4f25-8ab0-4174998474e6"}
+
+curl -X POST http://docker.local:8001/consumers/26550d58-68f0-4f25-8ab0-4174998474e6/oauth2 \
+    --data "name=Test%20Application" \
+    --data "redirect_uri=http://docker.local/login-redirect/"      
+
+{"consumer_id":"26550d58-68f0-4f25-8ab0-4174998474e6","client_id":"b4b123e18f3349e6bc7172a656692612","id":"7b196bc0-531b-47d9-a48c-bd605bdb949c","redirect_uri":"http:\/\/docker.local\/login-redirect\/","name":"Test Application","created_at":1462821732000,"client_secret":"10639ab4622147318f7910bcf7b7b460"}    
+
+curl http://docker.local:8000/oauth2/authorize \
+    --header "Host: service1.com" \
+    --header "x-forwarded-proto: https" \
+    --data "client_id=b4b123e18f3349e6bc7172a656692612" --data "response_type=code" --data "provision_key=c776b1cdd4974e85a7fc8e1390ce7769" --data "authenticated_userid=user123"
+
